b1b923f08bb3ecad6a28fb41dea7452b81bf1ff9
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-02-06 21:45:55 -0500

    Added a script to bring docker-compose down

---
+++ docker-down_env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export email_address='email@company.non'
+export email_password='email_password_change_me'
+export GID='1000'
+export host_ip='0.0.0.0'
+export hostname='localhost'
+export mysql_password='mysql_password_change_me'
+export mysql_root_password='mysql_root_password_change_me'
+export pihole_ip='127.0.0.1'
+export smbpassword='smbpassword_change_me'
+export smbuser='user'
+export smtp_host='email.hostname'
+export smtp_port='465'
+export UID='1000'
+echo -e "Set these variables for docker environments:
+email_address: $email_address
+email_password: $email_password
+GID: $GID
+host_ip: $host_ip
+hostname: $hostname
+mysql_password: $mysql_password
+mysql_root_password: $mysql_root_password
+pihole_ip: $pihole_ip
+smbpassword: $smbpassword
+smbuser: $smbuser
+smtp_host: $smtp_host
+smtp_port: $smtp_port
+UID: $UID"
+docker-compose down $1
