4ca3f5c2e5df10e9ad1a6ddac7cfed8b048203b4
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-02-06 21:45:52 -0500

    add more variables to docker-set_env.sh

--- docker-set_env.sh
+++ docker-set_env.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+export email_address='email@company.non'
+export email_password='change_me'
 export GID='1000'
 export hostname='localhost'
 export mysql_password='change_me'
 export mysql_root_password='change_me'
 export smbpassword='password'
 export smbuser='user'
+export smtp_host='email.hostname'
+export smtp_port='465'
 export UID='1000'
-echo -e "Set these variables for docker environments:\nGID: $GID\nhostname: $hostname\nmysql_password: $mysql_password\nmysql_root_password: $mysql_root_password\nsmbpassword: $smbpassword\nsmbuser: $smbuser\nUID: $UID"
+echo -e "Set these variables for docker environments:
+email_address: $email_address
+email_password: $email_password
+GID: $GID
+hostname: $hostname
+mysql_password: $mysql_password
+mysql_root_password: $mysql_root_password
+smbpassword: $smbpassword
+smbuser: $smbuser
+smtp_host: $smtp_host
+smtp_port: $smtp_port
+UID: $UID"
 docker-compose up -d --build
