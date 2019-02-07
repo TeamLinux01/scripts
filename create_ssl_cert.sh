823d1411db42d3b89513887780ddb73fa0600677
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-02-06 21:45:51 -0500

    SSL self signed cert generator

---
+++ create_ssl_cert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#creates a new ssl key, then adds it to the ssh agent
+#argument 1 ($1) server-hostname
+
+if [[ $# -ne 1 ]] ; then #display an error if both arguments are not entered
+    echo -e "Missing argument: server-hostname\nExample:\n\n$0 server-hostname\n"
+    exit 1
+fi
+
+openssl req -x509 -out $1.crt -keyout $1.key -newkey rsa:2048 -nodes -days 1825 -sha256
