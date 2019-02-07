6634db7519370973ebf64a0b1354f0684aa43952
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-02-06 21:45:52 -0500

    Added single quotes and UID GID

--- docker-set_env.sh
+++ docker-set_env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-export hostname=localhost
-export mysql_password=change_me
-export mysql_root_password=change_me
-export smbpassword=password
-export smbuser=user
-echo -e "Set these variables for docker environments:\nhostname: $hostname\nmysql_password: $mysql_password\nmysql_root_password: $mysql_root_password\nsmbpassword: $smbpassword\nsmbuser: $smbuser"
+export GID='1000'
+export hostname='localhost'
+export mysql_password='change_me'
+export mysql_root_password='change_me'
+export smbpassword='password'
+export smbuser='user'
+export UID='1000'
+echo -e "Set these variables for docker environments:\nGID: $GID\nhostname: $hostname\nmysql_password: $mysql_password\nmysql_root_password: $mysql_root_password\nsmbpassword: $smbpassword\nsmbuser: $smbuser\nUID: $UID"
 docker-compose up -d --build
