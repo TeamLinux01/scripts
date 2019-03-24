11c5a55310e7e4af794bb1772d71cfafc7753cd5
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-03-24 08:55:49 -0400

    Automatically safe-upgrade during Pop!_OS install

--- Pop_OS_Setup.sh
+++ Pop_OS_Setup.sh
@@ -87,7 +87,7 @@ else
     cp /tmp/docker-machine /usr/local/bin/docker-machine && printf "${LGREEN}DONE${NC}\n" && sleep 5;
 
   printf "${GREEN}Updating system.${NC}\n";
-  aptitude update && aptitude safe-upgrade && printf "${LGREEN}DONE${NC}\n" && sleep 5;
+  aptitude update && aptitude safe-upgrade -y && printf "${LGREEN}DONE${NC}\n" && sleep 5;
 
   printf "${GREEN}Installing Visual Studio Code (SNAP).${NC}\n";
   snap install vscode --classic && printf "${LGREEN}DONE${NC}\n" && sleep 5;
