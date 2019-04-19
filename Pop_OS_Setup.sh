f25f2a60a5f83246eb02096ca760c571934ec3f9
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-04-18 22:31:02 -0400

    Docker Compose 1.24.0

--- Pop_OS_Setup.sh
+++ Pop_OS_Setup.sh
@@ -8,7 +8,7 @@ NC='\033[0m'; #No Color
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ "$EUID" -ne 0 ];then
   printf "Usage:
     sudo ./Pop_OS_Setup.sh ${LGREEN}\$USER${NC} ${GREEN}vCompose vMachine${NC}
-    ${GREEN}vCompose${NC} format: ${LGREEN}1.23.2${NC}
+    ${GREEN}vCompose${NC} format: ${LGREEN}1.24.0${NC}
     ${GREEN}vMachine${NC} format: ${LGREEN}0.16.1${NC}
     Please run as ${RED}root${NC}\n";
   exit
