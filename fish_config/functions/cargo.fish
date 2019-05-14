7586379d505d2c9e531af7185c931181635b000e
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-05-14 13:38:18 -0400

    cargo.fish and rustc.fish now bindmount crates.io index

--- fish_config/functions/cargo.fish
+++ fish_config/functions/cargo.fish
@@ -1,3 +1,3 @@
 function cargo
-	docker run -it --rm -e USER=$USER -u (id -u $USER):(id -g $USER) -v (pwd):/projects -w /projects/ rust:latest cargo $argv
+	docker run -it --rm -e USER=$USER -u (id -u $USER):(id -g $USER) -v (pwd):/usr/src/myapp/ -w /usr/src/myapp/ -v (pwd)/../registry/:/usr/local/cargo/registry/ rust:latest cargo $argv
 end
