675863c4eb24393e017a9543785b22e930d8a40f
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-08-12 15:56:06 -0400

    docker-compose via container

--- dc_down.sh
+++ dc_down.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-docker-compose down $1;
+#!/bin/sh
+DOCKER_COMPOSE_VERSION="1.24.1"
+
+docker run --rm -it \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "${PWD}":"${PWD}" -w "${PWD}" \
+  docker/compose:$DOCKER_COMPOSE_VERSION down "$@"
