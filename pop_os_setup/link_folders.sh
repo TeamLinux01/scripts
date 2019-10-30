14b534e4cc37ac1dcd24a69e7350912c715c3e91
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2019-10-30 10:53:56 -0400

    Removed Steam and Lutris links

--- pop_os_setup/link_folders.sh
+++ pop_os_setup/link_folders.sh
@@ -5,30 +5,20 @@ LGREEN='\033[1;32m';
 RED='\033[0;31m';
 NC='\033[0m'; #No Color
 
-if [ -z "$1" ];then
-  printf "Usage:\n./link_folders.sh ${LGREEN}PATH_TO_PARENT_FOLDER_FOR_STEAM/GAMES${NC}\n";
-  exit
-else
-
-  printf "About to remove ~/Documents ~/Music ~/Pictures ~/Videos, create ~/Nextcloud, create symbolic links.\n";
-  printf "${LGREEN}$1${NC} is the path chosen for Steam and Games.\n\n";
-  read -p "Continue (y/n)?" choice
-  case "$choice" in
-  y|Y )
-      rmdir ~/Documents ~/Music ~/Pictures ~/Videos;
-      mkdir ~/Nextcloud;
-      ln -s ~/Nextcloud/Documents ~/Documents;
-      ln -s ~/Nextcloud/Music ~/Music;
-      ln -s ~/Nextcloud/Pictures ~/Pictures;
-      ln -s ~/Nextcloud/Videos ~/Videos;
-      ln -s $1/Steam ~/Steam;
-      ln -s $1/.steam ~/.steam;
-      ln -s $1/Games ~/Games;
-      ln -s $1/lutris ~/.local/share/lutris;
-      printf "Done.\n";;
-  n|N )
-      printf "${RED}Aborted.${NC}\n";;
-  * )
-      printf "${RED}Aborted.${NC}\n";;
-  esac;
-fi
+printf "About to remove ~/Documents ~/Music ~/Pictures ~/Videos, create ~/Nextcloud, create symbolic links.\n";
+printf "${LGREEN}$1${NC} is the path chosen for Steam and Games.\n\n";
+read -p "Continue (y/n)?" choice
+case "$choice" in
+y|Y )
+    rmdir ~/Documents ~/Music ~/Pictures ~/Videos;
+    mkdir ~/Nextcloud;
+    ln -s ~/Nextcloud/Documents ~/Documents;
+    ln -s ~/Nextcloud/Music ~/Music;
+    ln -s ~/Nextcloud/Pictures ~/Pictures;
+    ln -s ~/Nextcloud/Videos ~/Videos;
+    printf "Done.\n";;
+n|N )
+    printf "${RED}Aborted.${NC}\n";;
+* )
+    printf "${RED}Aborted.${NC}\n";;
+esac;
