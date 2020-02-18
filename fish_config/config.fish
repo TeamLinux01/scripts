50e936222063f12b625ff972840ceeb928fb8ada
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2020-02-18 14:57:03 -0500

    added fisher and fish-ssh-add

--- fish_config/config.fish
+++ fish_config/config.fish
@@ -1,1 +1,6 @@
 set -xU GPG_TTY (tty)
+if not functions -q fisher
+  set XDG_CONFIG_HOME ~/.config
+  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  fish -c fisher
+end
--- fish_config/functions/update.fish
+++
@@ -1,4 +0,0 @@
-function update
-	sudo aptitude update \
-	&& sudo aptitude safe-upgrade;
-end
