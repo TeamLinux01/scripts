30fbdcfcf363ce1372d0c18cfc57dfd995d634f6
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2020-03-02 11:32:41 -0500

    Removed KDE

--- pop_os_setup.sh
+++ pop_os_setup.sh
@@ -20,7 +20,7 @@ else
 
   ./pop_os_setup/removing_apps.sh
 
-  ./pop_os_setup/add_ppa_repos.sh flatpak.ppa git.ppa kubuntu-backports.ppa
+  ./pop_os_setup/add_ppa_repos.sh flatpak.ppa git.ppa
 
   printf "${GREEN}Updating the rest of the system.${NC}\n";
   apt dist-upgrade -y && printf "${LGREEN}DONE${NC}\n" && sleep 5;
