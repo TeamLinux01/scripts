39a18c9bce384237abeec31b3135fd6aa4340204
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2020-04-18 13:06:40 -0400

    Removed fish df command

--- fish_config/functions/df.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function df --description 'alias df=df -h --exclude-type=squashfs --exclude-type=tmpfs'
-	command df -h --exclude-type=squashfs --exclude-type=tmpfs $argv;
-end
