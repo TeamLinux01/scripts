d6adb6e1e92751b6a4d683a3bb66afb585d8fe58
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2020-05-03 14:24:39 -0400

    Flatpaks in Pop!_Shop are installed as user

--- pop_os_setup/add_flatpak_apps.sh
+++ pop_os_setup/add_flatpak_apps.sh
@@ -7,21 +7,9 @@ NC='\033[0m'; #No Color
 
 readarray -t FLATPAK_APPLICATIONS < `dirname $0`/lists/flatpak_apps.list;
 
-if [ "$EUID" -ne 0 ];then
-
-  printf "Usage:
-    sudo $0
-    Please run as ${RED}root${NC}\n";
-  exit
-else
-
-  for flatpak_app in ${FLATPAK_APPLICATIONS[@]}
-  do
-
-    printf "${GREEN}flatpak install flathub $flatpak_app${NC}\n";
-
-    flatpak install -y --noninteractive --user flathub $flatpak_app;
-  done
-
-  printf "\n${GREEN}$0${NC} ${LGREEN}Completed${NC}\n\n";
-fi
+for flatpak_app in ${FLATPAK_APPLICATIONS[@]}
+do
+  printf "${GREEN}flatpak install flathub $flatpak_app${NC}\n";
+  flatpak install -y --noninteractive --user flathub $flatpak_app;
+done
+printf "\n${GREEN}$0${NC} ${LGREEN}Completed${NC}\n\n";
